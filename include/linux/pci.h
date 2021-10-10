6f64e052f97b3a97952e344d110257ee9a8aca3c
Heisath <jannis@imserv.org>
2021-10-10 09:21:02 +0200

    09-pci-link-retraining(:1)
    Original-Subject: [ARCHEOLOGY] Add PCIe Wifi quirks patch for Atheros and Qualcomm cards
    > X-Git-Archeology: > recovered message: > https://lore.kernel.org/linux-pci/20210505163357.16012-1-pali@kernel.org/
    > X-Git-Archeology: - Revision 4b87543863cf296d01349b61d0620bf611ca1037: https://github.com/armbian/build/commit/4b87543863cf296d01349b61d0620bf611ca1037
    > X-Git-Archeology:   Date: Sun, 10 Oct 2021 09:21:02 +0200
    > X-Git-Archeology:   From: Heisath <jannis@imserv.org>
    > X-Git-Archeology:   Subject: Add PCIe Wifi quirks patch for Atheros and Qualcomm cards
    > X-Git-Archeology: 
    > X-Git-Archeology: - Revision 5a8513360cdcca63d26759f51ff9a670cbbcfb71: https://github.com/armbian/build/commit/5a8513360cdcca63d26759f51ff9a670cbbcfb71
    > X-Git-Archeology:   Date: Sun, 10 Oct 2021 21:30:56 +0200
    > X-Git-Archeology:   From: Heisath <jannis@imserv.org>
    > X-Git-Archeology:   Subject: Switch mvebu-edge to kernel 5.14
    > X-Git-Archeology: 
    > X-Git-Archeology: - Revision dd51f9f2afcbc83a3e10b32eb6a5061d91d1558e: https://github.com/armbian/build/commit/dd51f9f2afcbc83a3e10b32eb6a5061d91d1558e
    > X-Git-Archeology:   Date: Tue, 09 Nov 2021 18:06:34 +0100
    > X-Git-Archeology:   From: Igor Pecovnik <igorpecovnik@users.noreply.github.com>
    > X-Git-Archeology:   Subject: Bump imx6, xu4, rockchip64 and jetson-nano to 5.15 (#3238)
    > X-Git-Archeology: 
    > X-Git-Archeology: - Revision 9dbb72b01cfa61fe31532c57a456d1e760fa517e: https://github.com/armbian/build/commit/9dbb72b01cfa61fe31532c57a456d1e760fa517e
    > X-Git-Archeology:   Date: Sun, 21 Nov 2021 17:58:03 +0100
    > X-Git-Archeology:   From: Igor Pecovnik <igor.pecovnik@gmail.com>
    > X-Git-Archeology:   Subject: Adjusting patches due to upstream changes to make mvbe family operational again
    > X-Git-Archeology: 
    > X-Git-Archeology: - Revision 4de4c42e6f641eb6fc8eb02066486a7c42a7f73e: https://github.com/armbian/build/commit/4de4c42e6f641eb6fc8eb02066486a7c42a7f73e
    > X-Git-Archeology:   Date: Fri, 11 Feb 2022 11:00:57 +0100
    > X-Git-Archeology:   From: Heisath <jannis@imserv.org>
    > X-Git-Archeology:   Subject: Bump mvebu-current to 5.15 and mvebu-edge to 5.16
    > X-Git-Archeology: 
    > X-Git-Archeology: - Revision b619876210d23cc34652b4b4fa15cf7f61703079: https://github.com/armbian/build/commit/b619876210d23cc34652b4b4fa15cf7f61703079
    > X-Git-Archeology:   Date: Wed, 04 Jan 2023 18:37:42 +0100
    > X-Git-Archeology:   From: Jannis <52237708+Heisath@users.noreply.github.com>
    > X-Git-Archeology:   Subject: [AR-1313] Move mvebu edge to 6.1 (#4652)
    > X-Git-Archeology: 
    X-Armbian: Patch-File: 09-pci-link-retraining
    X-Armbian: Patch-File-Counter: 1
    X-Armbian: Patch-Rel-Directory: patch/kernel/archive/mvebu-5.15
    X-Armbian: Patch-Type: kernel
    X-Armbian: Patch-Root-Type: core
    X-Armbian: Patch-Sub-Type: common
    X-Armbian: Original-Subject: [ARCHEOLOGY] Add PCIe Wifi quirks patch for Atheros and Qualcomm cards

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -235,6 +235,8 @@ enum pci_dev_flags {
 	PCI_DEV_FLAGS_NO_RELAXED_ORDERING = (__force pci_dev_flags_t) (1 << 11),
 	/* Device does honor MSI masking despite saying otherwise */
 	PCI_DEV_FLAGS_HAS_MSI_MASKING = (__force pci_dev_flags_t) (1 << 12),
+	/* Don't Retrain Link for device when bridge is not in GEN1 mode */
+	PCI_DEV_FLAGS_NO_RETRAIN_LINK_WHEN_NOT_GEN1 = (__force pci_dev_flags_t) (1 << 12),
 };
 
 enum pci_irq_reroute_variant {
