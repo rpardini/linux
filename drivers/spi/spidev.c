225c92369e40c884952a2a0dc1679cdef3e1fd38
The-going <48602507+The-going@users.noreply.github.com>
2022-02-02 11:56:51 +0300

    drv-spi-spidev-remove-warnings(:1)
    Original-Subject: drv:spi:spidev remove warnings
    None
    X-Armbian: Patch-File: drv-spi-spidev-remove-warnings
    X-Armbian: Patch-File-Counter: 1
    X-Armbian: Patch-Rel-Directory: patch/kernel/archive/rockchip64-6.3
    X-Armbian: Patch-Type: kernel
    X-Armbian: Patch-Root-Type: core
    X-Armbian: Patch-Sub-Type: common
    X-Armbian: Original-Subject: drv:spi:spidev remove warnings

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -703,6 +703,7 @@ static const struct file_operations spidev_fops = {
 static struct class *spidev_class;
 
 static const struct spi_device_id spidev_spi_ids[] = {
+	{ .name = "spi-dev" },
 	{ .name = "dh2228fv" },
 	{ .name = "ltc2488" },
 	{ .name = "sx1301" },
@@ -731,6 +732,7 @@ static int spidev_of_check(struct device *dev)
 }
 
 static const struct of_device_id spidev_dt_ids[] = {
+	{ .compatible = "armbian,spi-dev", .data = &spidev_of_check },
 	{ .compatible = "cisco,spi-petra", .data = &spidev_of_check },
 	{ .compatible = "dh,dhcom-board", .data = &spidev_of_check },
 	{ .compatible = "lineartechnology,ltc2488", .data = &spidev_of_check },
