34c717ff5be50b5c038d8765c290a2dd22769bb6
John Doe <john.doe@somewhere.on.planet>
2024-08-14 16:33:07 +0000

    rockchip64: edge: 6.10.5 drv:spi:spidev remove warnings
    Signed-off-by: John Doe <john.doe@somewhere.on.planet>

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -700,6 +700,7 @@ static const struct class spidev_class = {
 };
 
 static const struct spi_device_id spidev_spi_ids[] = {
+	{ .name = "spi-dev" },
 	{ .name = "bh2228fv" },
 	{ .name = "dh2228fv" },
 	{ .name = "jg10309-01" },
@@ -730,6 +731,7 @@ static int spidev_of_check(struct device *dev)
 }
 
 static const struct of_device_id spidev_dt_ids[] = {
+	{ .compatible = "armbian,spi-dev", .data = &spidev_of_check },
 	{ .compatible = "cisco,spi-petra", .data = &spidev_of_check },
 	{ .compatible = "dh,dhcom-board", .data = &spidev_of_check },
 	{ .compatible = "elgin,jg10309-01", .data = &spidev_of_check },
