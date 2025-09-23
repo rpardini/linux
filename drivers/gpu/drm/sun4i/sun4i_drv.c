c8ffd8cf8a44b25d34396785569dd426dedcd7ad
Jernej Skrabec <jernej.skrabec@gmail.com>
2025-09-23 17:15:46 +0200

    drm/sun4i: Add compatible for H616 display engine
    Enable H616 display engine pipeline by adding respective compatible.
    
    Signed-off-by: Jernej Skrabec <jernej.skrabec@gmail.com>

--- drivers/gpu/drm/sun4i/sun4i_drv.c
+++ drivers/gpu/drm/sun4i/sun4i_drv.c
@@ -436,6 +436,7 @@ static const struct of_device_id sun4i_drv_of_table[] = {
 	{ .compatible = "allwinner,sun20i-d1-display-engine" },
 	{ .compatible = "allwinner,sun50i-a64-display-engine" },
 	{ .compatible = "allwinner,sun50i-h6-display-engine" },
+	{ .compatible = "allwinner,sun50i-h616-display-engine" },
 	{ }
 };
 MODULE_DEVICE_TABLE(of, sun4i_drv_of_table);
