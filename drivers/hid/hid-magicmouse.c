40bb4897fc976795923e6d4d4cf2b20139b4bd58
Aditya Garg <gargaditya08@live.com>
2025-10-08 01:48:18 +0000

    HID: magicmouse: fix regression breaking support for Magic Trackpad 1
    The case HID_ANY_ID and default are technically the same, but the first
    one was assigning no report to the Magic Trackpad 1, while the second
    one assigns the correct report. Since the first case is matched first,
    the Magic Trackpad 1 was not being assigned any report, breaking
    support for it.
    
    Signed-off-by: Aditya Garg <gargaditya08@live.com>

--- drivers/hid/hid-magicmouse.c
+++ drivers/hid/hid-magicmouse.c
@@ -1644,7 +1644,7 @@ static int magicmouse_probe(struct hid_device *hdev,
 		report = hid_register_report(hdev, HID_INPUT_REPORT,
 			TRACKPAD2_USB_REPORT_ID, 0);
 		break;
-	case HID_ANY_ID:
+	default:
 		switch (id->bus) {
 		case BUS_HOST:
 			report = hid_register_report(hdev, HID_INPUT_REPORT, MTP_REPORT_ID, 0);
@@ -1652,15 +1652,12 @@ static int magicmouse_probe(struct hid_device *hdev,
 		case BUS_SPI:
 			report = hid_register_report(hdev, HID_INPUT_REPORT, SPI_REPORT_ID, 0);
 			break;
-		default:
-			break;
+		default: /* USB_DEVICE_ID_APPLE_MAGICTRACKPAD */
+			report = hid_register_report(hdev, HID_INPUT_REPORT,
+				TRACKPAD_REPORT_ID, 0);
+			report = hid_register_report(hdev, HID_INPUT_REPORT,
+				DOUBLE_REPORT_ID, 0);
 		}
-		break;
-	default: /* USB_DEVICE_ID_APPLE_MAGICTRACKPAD */
-		report = hid_register_report(hdev, HID_INPUT_REPORT,
-			TRACKPAD_REPORT_ID, 0);
-		report = hid_register_report(hdev, HID_INPUT_REPORT,
-			DOUBLE_REPORT_ID, 0);
 	}
 
 	if (!report) {
