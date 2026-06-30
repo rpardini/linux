839eeea25b3e29f09c641dd53a7376d6081a8908
retro98boy <retro98boy@qq.com>
2026-06-30 16:07:14 +0800

    drv: net: phy: Add maxio phys driver
    https://github.com/zuyih/linux-cubie/blob/c5ea47e5f899042abe54437ced1fd49ac1226582/drivers/net/phy/maxio.c

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -56,6 +56,9 @@
 #include "dwxgmac2.h"
 #include "hwif.h"
 
+#define MAXIO_PHY_MAE0621A_Q2C_ID 0x7b744411
+#define MAXIO_PHY_MAE0621A_Q3C_ID 0x7b744412
+
 /* As long as the interface is active, we keep the timestamping counter enabled
  * with fine resolution and binary rollover. This avoid non-monotonic behavior
  * (clock jumps) when changing timestamping settings at runtime.
@@ -8294,6 +8297,22 @@ int stmmac_resume(struct device *dev)
 
 	stmmac_free_tx_skbufs(priv);
 	stmmac_clear_descriptors(priv, &priv->dma_conf);
+
+	/*
+	 * Provide RX CLK for Rockchip's DWMAC otherwise the controller will
+	 * fail to initialize with "DMA engine initialization failed".
+	 */
+	if (ndev->phydev) {
+		if ((ndev->phydev->phy_id == MAXIO_PHY_MAE0621A_Q2C_ID ||
+		     ndev->phydev->phy_id == MAXIO_PHY_MAE0621A_Q3C_ID)) {
+			ret = phy_init_hw(ndev->phydev);
+			if (ret) {
+				netdev_err(priv->dev,
+					   "PHY initialization failed: %d\n",
+					   ret);
+			}
+		}
+	}
 
 	ret = stmmac_hw_setup(ndev);
 	if (ret < 0) {
